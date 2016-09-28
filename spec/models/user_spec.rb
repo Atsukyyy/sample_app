8a97e6dc03ce4e27b78d8c49af3f423f36ae7ef4
Atsuky <a.mitsuyori.15@gmail.com>
2016-09-28 21:56:33 +0900

    7章完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,7 @@ describe User do
   it { should respond_to(:password_confirmation) }  
   it { should respond_to(:authenticate) }
   
-  it { shuold be_valid }
+  it { should be_valid }
   
   describe "when name is not present" do
   	before { @user.name = " " }
