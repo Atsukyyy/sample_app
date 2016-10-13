ec525392e619c3d0ea9e9e970d006a0f3838e5ec
Atsuky <a.mitsuyori.15@gmail.com>
2016-10-13 23:22:34 +0900

    11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+	has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
-
 	validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
@@ -22,6 +28,18 @@ class User < ActiveRecord::Base
 
 	def feed
     Micropost.where("user_id = ?", id)
+  end
+
+	def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+	def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 
   private
